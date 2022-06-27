f4dc8e9bb6fa27f33ea19954b39e636ac772722c
hainh-0199 <44605732+hainh184088@users.noreply.github.com>
2022-06-27 05:06:02 +0000

    save todo on Firestone

---
+++ bash.sh
@@ -0,0 +1,7 @@
+curl o https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 16.15.0
+npm install
+npm install firebase-tools --global
+firebase login --no-localhost
